856aaf0300b61006e3565977975e52a49d9481d5
kyle103507 <61506510+kyle103507@users.noreply.github.com>
2020-07-23 21:35:45 +0800

    遊戲切換和離開遊戲場景

--- 2Dpan/Assets/Script/Player.cs
+++ 2Dpan/Assets/Script/Player.cs
@@ -2,7 +2,66 @@
 
 public class Player : MonoBehaviour
 {
- 
+    #region 欄位區域
+    [Header("速度"),Tooltip("角色的移動速度"),Range(10,500)]
+    public float speed;
+    [Header("血量")]
+    public float hp;
+    [Header("跳躍高度")]
+    public float jamphight;
+    [Header("破關層數"),Tooltip("角色往下幾層")]
+    public int LV;
+    [Header("碰撞音效")]
+    public AudioClip audcol;
+    [Header("跳躍音效")]
+    public AudioClip audjump;
+    [Header("是否死亡")]
+    private bool dead;
+    #endregion
+
+    #region 方法區域
     
+    /// <summary>
+    ///角色的移動方式
+    /// </summary>
+    private void Move()
+    {
+
+    }
     
+    /// <summary>
+    /// 角色的跳躍:碰到彈簧跳躍、跳躍的音效、動畫
+    /// </summary>
+    private void Jump()
+    {
+
+    }
+
+    /// <summary>
+    /// 回復血量:碰撞地板回復、碰撞音效
+    /// </summary>
+    private void Reply()
+    {
+
+    }
+    
+    /// <summary>
+    /// 角色受傷:碰到陷阱、受傷音效、動畫
+    /// </summary>
+    private void Hit()
+    {
+
+    }
+
+    /// <summary>
+    /// 角色死亡:離開畫面、動畫
+    /// </summary>
+    private void Dead()
+    {
+
+    }
+
+    #endregion
+
+
 }
---
+++ 2Dpan/Assets/Script/ScenceControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ScenceControl : MonoBehaviour
+{
+
+
+    public void Quit()
+    {
+        Application.Quit();                          //應用程式.離開
+    }
+   
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("遊戲背景");          //切換場景
+    }
+       
+    public void DelayQuit()
+    {
+        Invoke("Quit", 0.8f);                        //延遲離開場景
+    }
+    
+    public  void DelayLoadScene()
+    {
+        Invoke("LoadScene", 0.8f);                      //延遲切換場景
+    }
+
+}
+
+
